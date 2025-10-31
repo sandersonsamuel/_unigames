ca70f38873a2d8afd52b337fa8d351943bcf18ec
Samuel Sanderson <sandersonsamuel401@gmail.com>
2025-10-30 23:52:52 -0300

    refactor: remover o uso do roleGuard no gerenciamento de compras

--- apps/api/src/routes/purchases.routes.ts
+++ apps/api/src/routes/purchases.routes.ts
@@ -2,10 +2,8 @@ import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import z from "zod/v4";
 import { competitorsSchema } from "../schemas/competitors.schema";
 import { purchaseWithGameSchema } from '../schemas/purchases.schema';
-import { createPurchase, getPurchasesByUserId, updatePurchase } from '../services/purchases.service';
 import { createPreference } from "../services/preference.service";
-import { roleGuard } from "../plugins/role-guard.plugin";
-import { Role } from "../types/role.types";
+import { createPurchase, getPurchasesByUserId, updatePurchase } from '../services/purchases.service';
 import { SupabaseJWT } from "../types/supabase.types";
 
 export const purchaseRoutes: FastifyPluginAsyncZod = async (app) => {
@@ -28,8 +26,7 @@ export const purchaseRoutes: FastifyPluginAsyncZod = async (app) => {
             message: z.string(),
           }),
         },
-      },
-      preHandler: [roleGuard([Role.GAMER])]
+      }
     }, async (request, reply) => {
 
       const { competitors, email, gameId } = request.body;
@@ -62,8 +59,7 @@ export const purchaseRoutes: FastifyPluginAsyncZod = async (app) => {
       response: { 200: z.array(purchaseWithGameSchema) },
       tags: ["Purchases"],
       summary: "Get purchases by userId",
-    },
-    preHandler: [roleGuard([Role.GAMER])]
+    }
   }, async (request, reply) => {
     const { sub: userId } = request.user as SupabaseJWT
     const purchases = await getPurchasesByUserId(userId);
